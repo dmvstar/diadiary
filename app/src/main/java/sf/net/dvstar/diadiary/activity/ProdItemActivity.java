be6d4ef9fff70aeab7940a8d85858e78e3ae78ff
Dmitry Starzhynskyi <dmvstar@gmail.com>
2015-11-30 16:29:05 +0200

    add translations, refactoring

--- app/src/main/java/sf/net/dvstar/diadiary/activity/ProdItemActivity.java
+++ app/src/main/java/sf/net/dvstar/diadiary/activity/ProdItemActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -13,15 +14,13 @@ import com.activeandroid.query.Select;
 import java.util.List;
 
 import sf.net.dvstar.diadiary.R;
-import sf.net.dvstar.diadiary.database.ProductGroup;
 import sf.net.dvstar.diadiary.database.ProductItem;
 
 import sf.net.dvstar.diadiary.utilitis.CommonConstants;
 
 public class ProdItemActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
-    ListView mProdGroups;
-
+    ListView mProductItems;
 
     /*
     String query = Client.all().orderBy("name").toSql();
@@ -42,7 +41,7 @@ public class ProdItemActivity extends AppCompatActivity implements AdapterView.O
         if(getIntent().getExtras()!=null)
             iId = getIntent().getExtras().getLong(CommonConstants.KEY_INTENT_EXTRA_ROW_ID);
 
-        mProdGroups = (ListView) findViewById(R.id.lv_prod_items);
+        mProductItems = (ListView) findViewById(R.id.lv_prod_items);
         List<ProductItem> list;
         if(iId>=0)
             list = new Select().from(ProductItem.class).where("groupId = ?",iId).execute();
@@ -50,14 +49,16 @@ public class ProdItemActivity extends AppCompatActivity implements AdapterView.O
             list = new Select().from(ProductItem.class).execute();
 
         ArrayAdapter adapter = new ArrayAdapter<ProductItem>(this,android.R.layout.simple_list_item_1, list);
-        mProdGroups.setAdapter(adapter);
-        mProdGroups.setOnItemClickListener(this);
+        mProductItems.setAdapter(adapter);
+        mProductItems.setOnItemClickListener(this);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String product = mProdGroups.getAdapter().getItem(position).toString();
+        String product = mProductItems.getAdapter().getItem(position).toString();
         Intent intent = new Intent();
         intent.putExtra(CommonConstants.KEY_INTENT_EXTRA_GET_PRODUCT, product);
         setResult(RESULT_OK, intent);
