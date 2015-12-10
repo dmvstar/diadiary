419e1205c2b5a5cad1afdce8ac1e10acf2e77f29
Dmitry <dmvstar@gmail.com>
2015-12-10 21:42:24 +0200

    add filter for products and select it, correcting code

--- app/src/main/java/sf/net/dvstar/diadiary/adapters/ProductFilterAdapter.java
+++ app/src/main/java/sf/net/dvstar/diadiary/adapters/ProductFilterAdapter.java
@@ -18,6 +18,7 @@ import java.util.List;
 import sf.net.dvstar.diadiary.R;
 import sf.net.dvstar.diadiary.database.ProductGroup;
 import sf.net.dvstar.diadiary.database.ProductItem;
+import sf.net.dvstar.diadiary.utilitis.CommonUtils;
 
 public class ProductFilterAdapter extends ArrayAdapter<ProductItem> {
 
@@ -34,18 +35,16 @@ public class ProductFilterAdapter extends ArrayAdapter<ProductItem> {
         this.mDataOriginal = aData;
         this.mDataFiltered = new ArrayList<>(aData);
     }
-//
-//    public void addFilter(String aFilter){
-//        if(!mFilter.equals(aFilter)) {
-//            mFilter = aFilter;
-//        }
-//
-//    }
 
     static class ProductHolder
     {
         TextView tvName;
         TextView tvMenu;
+
+        TextView tvCarb;
+        TextView tvFats;
+        TextView tvProt;
+        TextView tvXe;
     }
 
     @Override
@@ -62,6 +61,11 @@ public class ProductFilterAdapter extends ArrayAdapter<ProductItem> {
             holder.tvName = (TextView)row.findViewById(R.id.item_product_label);
             holder.tvMenu = (TextView)row.findViewById(R.id.item_product_menu);
 
+            holder.tvCarb = (TextView)row.findViewById(R.id.tv_prod_carb);
+            holder.tvFats = (TextView)row.findViewById(R.id.tv_prod_fat);
+            holder.tvProt = (TextView)row.findViewById(R.id.tv_prod_prot);
+            holder.tvXe = (TextView)row.findViewById(R.id.tv_prod_xe);
+
             row.setTag(holder);
         }
         else
@@ -69,23 +73,26 @@ public class ProductFilterAdapter extends ArrayAdapter<ProductItem> {
             holder = (ProductHolder)row.getTag();
         }
 
-        ProductItem product = mDataOriginal.get(position);
+        ProductItem product = mDataFiltered.get(position);
 
         holder.tvName.setText(product.name);
         ProductGroup group = new Select().from(ProductGroup.class).where("groupId = ?", product.groupId).executeSingle();
         holder.tvMenu.setText(group.name);
-
+        holder.tvCarb.setText(CommonUtils.getFloatString2Decimal(product.carb));
+        holder.tvFats.setText(CommonUtils.getFloatString2Decimal(product.fats));
+        holder.tvProt.setText(CommonUtils.getFloatString2Decimal(product.prot));
+        holder.tvXe.setText(""+(int) (product.carb / 12.0));
         return row;
     }
 
     @Override
     public int getCount() {
-        return mDataOriginal.size();
+        return mDataFiltered.size();
     }
 
     @Override
     public ProductItem getItem(int position) {
-        return mDataOriginal.get(position);
+        return mDataFiltered.get(position);
     }
 
     @Override
@@ -128,7 +135,7 @@ public class ProductFilterAdapter extends ArrayAdapter<ProductItem> {
         @SuppressWarnings("unchecked")
         @Override
         protected void publishResults(CharSequence constraint, FilterResults results) {
-            mDataOriginal = (ArrayList<ProductItem>) results.values; // has
+            mDataFiltered = (ArrayList<ProductItem>) results.values; // has
             notifyDataSetChanged();
         }
 
