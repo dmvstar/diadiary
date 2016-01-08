5966a2ad48cabfdb1a6a96065fe37a5e6b8b9fb1
Dmitry <dmvstar@gmail.com>
2016-01-08 11:20:18 +0200

    refactoring code

--- app/src/main/java/sf/net/dvstar/diadiary/activity/ActivitySaving.java
+++ app/src/main/java/sf/net/dvstar/diadiary/activity/ActivitySaving.java
@@ -1,8 +1,12 @@
 package sf.net.dvstar.diadiary.activity;
 
+import android.view.View;
+
 public interface ActivitySaving {
 
     public void fillFieldData();
     public void saveFieldData();
+    public void cancel(View view);
+    public void confirm(View view);
 
 }
--- app/src/main/java/sf/net/dvstar/diadiary/activity/ConfigParamsActivity.java
+++ app/src/main/java/sf/net/dvstar/diadiary/activity/ConfigParamsActivity.java
@@ -3,10 +3,11 @@ package sf.net.dvstar.diadiary.activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import sf.net.dvstar.diadiary.R;
 
-public class ConfigParamsActivity extends AppCompatActivity {
+public class ConfigParamsActivity extends AppCompatActivity implements ActivitySaving{
 
     private Context mContext;
 
@@ -16,4 +17,26 @@ public class ConfigParamsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_config_params);
         mContext = this;
     }
+
+    @Override
+    public void fillFieldData() {
+
+    }
+
+    @Override
+    public void saveFieldData() {
+
+    }
+
+    @Override
+    public void cancel(View view) {
+        finish();
+    }
+
+    @Override
+    public void confirm(View view) {
+        finish();
+    }
+
+
 }
