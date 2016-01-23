a6198d85e22cc392abca083b329eabd8cda0394c
Dmitry <dmvstar@gmail.com>
2016-01-23 12:48:30 +0200

    refactoring code add theme definitions

--- app/src/main/java/sf/net/dvstar/diadiary/activity/ConfigParamsActivity.java
+++ app/src/main/java/sf/net/dvstar/diadiary/activity/ConfigParamsActivity.java
@@ -1,21 +1,30 @@
 package sf.net.dvstar.diadiary.activity;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import sf.net.dvstar.diadiary.R;
 
-public class ConfigParamsActivity extends AppCompatActivity implements ActivitySaving{
+public class ConfigParamsActivity extends AppCompatActivity implements ActivitySaving, AdapterView.OnItemSelectedListener {
 
     private Context mContext;
+    private Spinner mSpSelectTheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config_params);
         mContext = this;
+
+        mSpSelectTheme = (Spinner) findViewById(R.id.sp_config_global_theme);
+        mSpSelectTheme.setOnItemSelectedListener(this);
+
     }
 
     @Override
@@ -39,4 +48,26 @@ public class ConfigParamsActivity extends AppCompatActivity implements ActivityS
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        Toast.makeText(getBaseContext(), "Position = " + position+" "+getApplication().getTheme().toString(), Toast.LENGTH_SHORT).show();
+        switch(position){
+            case 0: // dark
+                getApplication().setTheme(R.style.AppTheme_Dark_NoActionBar);
+                getApplicationContext().setTheme(R.style.AppTheme_Dark_NoActionBar);
+                setTheme(R.style.AppTheme_Dark_NoActionBar);
+                break;
+            case 1: // light
+                getApplication().setTheme(R.style.AppTheme_Light_NoActionBar);
+                getApplicationContext().setTheme(R.style.AppTheme_Light_NoActionBar);
+                setTheme(R.style.AppTheme_Light_NoActionBar);
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
